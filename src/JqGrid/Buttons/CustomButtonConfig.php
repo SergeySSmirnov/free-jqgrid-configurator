3402824df8b3d6e506a9123ff4d60a973bac4c74
Sergey S. Smirnov <SergeySSmirnov@mail.ru>
2020-10-29 11:35:05 +0300

    Removed empty fields from CustomButton config.
    Signed-off-by: Sergey S. Smirnov <SergeySSmirnov@mail.ru>

--- src/JqGrid/Buttons/CustomButtonConfig.php
+++ src/JqGrid/Buttons/CustomButtonConfig.php
@@ -110,7 +110,10 @@ class CustomButtonConfig implements ConfigurationDefinitionInterface
 
         foreach ($this as $_key => $_val) {
             if (is_string($_val)) {
-                $_config[$_key] = trim($_val);
+
+                if (!empty($_val)) {
+                    $_config[$_key] = trim($_val);
+                }
             } elseif (is_null($_val)) {
                 continue;
             } else {
